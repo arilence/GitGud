da238799fadc07a8381e9c710404a5b67f9075cb
Anthony Smith <anthony@smith.lt>
2015-06-30 13:34:52 -0700

    Implement username form and include handlebarsJS

--- contribute.js
+++ contribute.js
@@ -1,14 +1,49 @@
+var LOCAL_STREAK_KEY = "contributionStreak";
+var LOCAL_USERNAME_KEY = "githubUsername";
+
 $(document).ready(function() {
-  setupLocalization();
-  getContributionData();
+  // localStorage.removeItem(LOCAL_STREAK_KEY);
+  // localStorage.removeItem(LOCAL_USERNAME_KEY);
+  renderView();
+  setupText();
+
+  $("#ask-form").submit(function( event ) {
+    var inputUsername = $("#ask-form-username").val();
+    if (inputUsername.length != 0) {
+      saveUsername(inputUsername);
+      renderView();
+    }
+    event.preventDefault();
+  });
 });
 
-var setupLocalization = function() {
-  $("#text").html(chrome.i18n.getMessage("streakText"));
+var renderView = function() {
+  if (loadUsername() === null) {
+    var source   = $("script#ask-form").html();
+  } else {
+    var source   = $("script#streak").html();
+    getContributionData();
+  }
+
+  var template = Handlebars.compile(source);
+  var html = template();
+  $("#display").html(html);
+}
+
+var setupText = function() {
+  $("#days").html(loadStreak());
+
+  // Loops through the page and replace all text with locale compatible fields
+  $('[data-locale]').each(function() {
+    var el = $(this);
+    var resourceName = el.data('locale');
+    var resourceText = chrome.i18n.getMessage(resourceName);
+    el.text(resourceText);
+  });
 }
 
 var getContributionData = function() {
-  $.get('https://github.com/users/lawsmith/contributions')
+  $.get('https://github.com/users/' + loadUsername() + '/contributions')
     .done(function(data) {
       calculateStreak(data);
     })
@@ -35,7 +70,24 @@ var calculateStreak = function(contribution) {
     }
   }
 
+  saveStreak(count);
   showStreak(count);
+}
+
+var loadUsername = function() {
+  return localStorage.getItem(LOCAL_USERNAME_KEY);
+}
+
+var saveUsername = function(username) {
+  localStorage.setItem(LOCAL_USERNAME_KEY, username);
+}
+
+var loadStreak = function() {
+  return localStorage.getItem(LOCAL_STREAK_KEY);
+}
+
+var saveStreak = function(count) {
+  localStorage.setItem(LOCAL_STREAK_KEY, count);
 }
 
 var showStreak = function(count) {
